d42092228b46f188356c7aa5796f6ad55b530af0
johannamaria91 <johanna.agnar@outlook.com>
2021-10-04 15:43:58 +0200

    port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express'); 
 const app = express(); 
 const cors = require('cors');
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 const { Router } = require('express');
 const router = new Router();
 const fs = require('fs');
